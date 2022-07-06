89d6cea283db13d07e937159b6e1e164795b156d
Gabriele <gabrieletuozzo@gmail.com>
2022-07-06 12:29:53 +0200

    updated version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="kg-qa",
-    version="0.1.3",
+    version="0.1.4",
     description="Library that allows to perform Knowledge Graph (Linked Open Data) quality analysis.",
     long_description=long_description,
     long_description_content_type="text/markdown",
